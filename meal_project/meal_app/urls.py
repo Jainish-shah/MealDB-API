69dd5d406bbf161fda43907cf9a111f765eca033
Jainish <jainishnikul2525@gmail.com>
2022-01-28 18:37:34 +0530

    creating urls.py in meal_app

---
+++ meal_project/meal_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import URLPattern, path
+from product_app import views
+
+urlpatterns = [
+    path('', views.get_meals, name = "get_meals"),
+    path('meals/<int:id>/', views.meal_detail, name = "meal_detail")
+]
